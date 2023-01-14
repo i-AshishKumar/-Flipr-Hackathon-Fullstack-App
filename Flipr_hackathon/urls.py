22ad4697bccabd94ceec1e07bf3cb1f40efe9567
i-AshishKumar <ashishkumarg0877@gmail.com>
2023-01-14 14:27:00 +0530

    initial-push

--- Flipr_hackathon/urls.py
+++ Flipr_hackathon/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('apis.urls')),
     path('',include('authentication.urls')),
+    path('',include('frontend.urls'))
 ]
 
